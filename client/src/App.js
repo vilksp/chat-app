49dc03774a803cf413849884b18f00cb38ff7159
vilksp <v.kasperavicius1@gmail.com>
2022-01-21 20:09:24 +0200

    Client is able to set Username and room name

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,37 @@
 import "./App.css";
+import io from "socket.io-client";
+import React, { useState } from "react";
+
+const socket = io.connect("http://localhost:3001");
 
 function App() {
+	const [username, setUsername] = useState("");
+	const [room, setRoom] = useState("");
+
+	const joinRoom = () => {
+		if (username !== "" && room !== "") {
+			socket.emit("join_room", room);
+		}
+	};
 	return (
-		<div className="Chat App">
-			<h3>name</h3>
+		<div className="App">
+			<h3>Join a chat</h3>
+			<input
+				type="text"
+				placeholder="Type your name..."
+				onChange={(e) => {
+					setUsername(e.target.value);
+				}}
+			/>
+			<h3>Join a room</h3>
+			<input
+				type="text"
+				placeholder="Type a room name to join"
+				onChange={(e) => {
+					setRoom(e.target.value);
+				}}
+			/>
+			<button onClick={joinRoom}>Join a room</button>
 		</div>
 	);
 }

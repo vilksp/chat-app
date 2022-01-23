940ba5387417bc6ddd724dd9f0d7ed06654b9aa4
vilksp <v.kasperavicius1@gmail.com>
2022-01-23 16:16:03 +0200

    Send message function added

--- server/index.js
+++ server/index.js
@@ -22,6 +22,10 @@ io.on("connection", (socket) => {
 		console.log(`User with ID: ${socket.id} joined a room: ${data}`);
 	});
 
+	socket.on("send_message", (data) => {
+		console.log(data);
+	});
+
 	socket.on("disconnect", () => {
 		console.log(`User disconnected:  ${socket.id}`);
 	});

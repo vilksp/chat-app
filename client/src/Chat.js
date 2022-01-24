73109c3f29f6bb640a6ccbfe0d1763a387b3616f
vilksp <v.kasperavicius1@gmail.com>
2022-01-24 12:12:40 +0200

    able to send basic msg

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Chat({ socket, username, room }) {
+	const [currentMessage, setCurrentMessage] = useState("");
+
+	const sendMessage = async () => {
+		if (currentMessage !== "") {
+			const messageData = {
+				room: room,
+				author: username,
+				message: currentMessage,
+				time:
+					new Date(Date.now()).getHours() +
+					":" +
+					new Date(Date.now()).getMinutes(),
+			};
+			await socket.emit("send_message", messageData);
+		}
+	};
 	return (
 		<div>
 			<div className="chat-header">
@@ -8,8 +24,14 @@ function Chat({ socket, username, room }) {
 			</div>
 			<div className="chat-body"></div>
 			<div className="chat-footer">
-				<input input="text" placeholder="Hey..." />
-				<button>&#9658;</button>
+				<input
+					input="text"
+					placeholder="Hey..."
+					onChange={(e) => {
+						setCurrentMessage(e.target.value);
+					}}
+				/>
+				<button onClick={sendMessage}>&#9658;</button>
 			</div>
 		</div>
 	);
